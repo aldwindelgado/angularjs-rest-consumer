a78c72d26e9d4d9e9af3bde6499bc3971edcf903
Aldwin Delgado Dev-02@DevKinetics <aldwinpaolodelgado@gmail.com>
2018-02-09 18:12:45 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import {HttpClient} from '@angular/common/http';
+
+interface AdminResponseType {
+    message?: string;
+    returnCode?: number;
+}
+
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
 export class AppComponent {
-  title = 'app works!';
+    message: string;
+    returnCode: number;
+    urlInput = 'http://localhost:7002/v1/auth/oauth2/testing-cors';
+
+    constructor(private http: HttpClient) {
+        this.consumeRestFull();
+    }
+
+    consumeRest() {
+        console.log('consume rest');
+        this
+            .http
+            .get(this.urlInput)
+            .subscribe((Response: any) => {
+                console.log('SUCCESS RESPONSE:', Response);
+                this.message = Response.message;
+                this.returnCode = Response.returnCode;
+            }, (error) => {
+                console.log('ERROR RESPONSE:', error);
+            });
+    }
+
+    consumeRestFull() {
+        console.log('consume rest with headers');
+        this
+            .http
+            .get<AdminResponseType>(this.urlInput, {observe: 'response'})
+            .subscribe( Response => {
+                console.log('SUCCESS RESPONSE:', Response);
+                const headerKeys = Response.headers.keys();
+                const headersShit = headerKeys.map(
+                    key => `${key}: ${Response.headers.get(key)}`
+                );
+                console.log('SUCCESS RESPONSE HEADERS:', headersShit);
+
+                this.message = Response.body.message;
+                this.returnCode = Response.body.returnCode;
+            }, (error) => {
+                console.log('ERROR RESPONSE:', error);
+            });
+    }
 }
